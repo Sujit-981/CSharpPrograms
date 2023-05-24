1ef383249b22d450c825c8f33192060e03058346
Sujit <sujitsahukar18@gmail.com>
2023-05-24 15:43:27 +0530

    first commit

---
+++ GreatestProduct/GreatestProduct/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatestProduct
+{
+    class Program
+    {
+        //method to find the product of four digits in a number.
+        public int Product(int num)
+        {
+            int result = 1;
+            while (num != 0)
+            {
+
+                result = result * (num % 10);
+                num = num / 10; 
+
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number which is greater than 999.");
+            string number = Console.ReadLine();
+            int greatest = 0;
+
+            if (number.Length < 4)
+            {
+                Console.WriteLine("Invalid Input, Enter a valid one.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Program obj = new Program();
+
+                //iterating through every four digit numbers in the number variable, to find the greatest product.
+                for (int i = 0; i < number.Length - 3; i++)
+                {
+                    int check = obj.Product(Convert.ToInt32(number.Substring(i, 4)));
+                    if (greatest < check)
+                    {
+                        greatest = check;
+                    }
+                }
+
+                Console.WriteLine($"The greatest possible product here is {greatest}");
+                Console.ReadKey();
+            }
+            
+
+        }
+    }
+}
---
+++ StringSubString/StringSubString/TestCases.md
@@ -0,0 +1,24 @@
+TestCases: 
+
+Test Case 1:
+Input :
+asfkvjnfrduygfjashfrd;ihsfvfrdoishfrd
+frd
+Output:
+The substring str2 is found at indexes: 7 18 27 34
+Total occurrences of 'frd' in 'asfkvjnfrduygfjashfrd;ihsfvfrdoishfrd': 4	
+
+Test Case 2:
+Input :
+vjnfrduygfjashfrd;ihsdoishfrd
+09hg
+Output:
+There is no substring str2 in str1
+
+Test Case 3:
+Input :
+herdftevuygfuefuefueeeereeeeee
+e
+Output:
+The substring str2 is found at indexes: 1 6 13 16 19 20 21 22 24 25 26 27 28 29
+Total occurrences of 'e' in 'herdftevuygfuefuefueeeereeeeee': 14
